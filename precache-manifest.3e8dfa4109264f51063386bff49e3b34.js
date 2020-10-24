c1ce8ea004b134af76a16100d93a84a6f0312f62
Liubomyr <43443223+LiubomyrChereshchuk@users.noreply.github.com>
2020-10-24 15:51:22 +0300

    Updates

---
+++ precache-manifest.3e8dfa4109264f51063386bff49e3b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfd06a44b139fb736d36a65da6cb00c",
+    "url": "/test-task-pokemon/index.html"
+  },
+  {
+    "revision": "cefb080d38f4d426b998",
+    "url": "/test-task-pokemon/static/css/main.c3e4c0f7.chunk.css"
+  },
+  {
+    "revision": "f36820d5695d28a3cb9a",
+    "url": "/test-task-pokemon/static/js/2.bf159806.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/test-task-pokemon/static/js/2.bf159806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefb080d38f4d426b998",
+    "url": "/test-task-pokemon/static/js/main.e37eeee5.chunk.js"
+  },
+  {
+    "revision": "23298ea83e1cca94a84b",
+    "url": "/test-task-pokemon/static/js/runtime-main.b1fb303d.js"
+  }
+]);
